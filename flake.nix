4b7bf8ac5da6a46e22b8a2043ddbe02a70fffce2
Bohdan <b.liesnikov+git@gmail.com>
2024-05-08 13:45:00 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "literate programming filters for Agda and Haskell";
+
+  inputs.nixpkgs.url = github:NixOS/nixpkgs;
+  inputs.flake-utils.url = github:numtide/flake-utils;
+
+  outputs = {self, nixpkgs, flake-utils}:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {inherit system;};
+        filters = pkgs.haskellPackages.callCabal2nix "pandoc-literate-filters" ./. {};
+      in {
+        packages = {
+          default = filters;
+        };
+        devShells.default = pkgs.haskellPackages.shellFor {
+          packages = p: [filters];
+          buildInputs = with pkgs.haskellPackages; [
+            cabal-install
+            cabal2nix
+            haskell-language-server
+            pandoc
+          ];
+        };
+      });
+}
