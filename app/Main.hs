d9e9aa666c4856b27f50bf9510f98f1ccebe19f1
Bohdan <b.liesnikov+git@gmail.com>
2024-05-08 15:03:04 +0200

    add an agda filter

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Data.Text (Text)
+import Text.Pandoc.JSON
+
+default (Text, String)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = toJSONFilter literateAgda
+
+literateAgda :: Block -> IO Block
+literateAgda (CodeBlock (_, ["agda"], _) t) = do
+  pure $ RawBlock (Format "latex") ("\\begin{code}" <> "\n" <> t <> "\n" <> "\\end{code}")
+literateAgda (CodeBlock (_, ["agda", "hide"], _) t) = do
+  pure $ RawBlock (Format "latex") ("\\begin{code}[hide]" <> "\n" <> t <> "\n" <> "\\end{code}")
+literateAgda x = return x
